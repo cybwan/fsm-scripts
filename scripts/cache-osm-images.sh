0f0f8c819bae3e2305693f839c68abec682c0c2b
Cybwan <baili@flomesh.io>
2022-08-18 14:23:40 +0800

    Optimized cache-osm-images.sh

--- scripts/cache-osm-images.sh
+++ scripts/cache-osm-images.sh
@@ -32,5 +32,5 @@ sed -i 's#image: prom/prometheus:v2.18.1#image: localhost:5000/prom/prometheus:v
 sed -i 's#image: grafana/grafana:8.2.2#image: localhost:5000/grafana/grafana:8.2.2#g' "${OSM_HOME}"/charts/osm/values.yaml
 sed -i 's#rendererImage: grafana/grafana-image-renderer:3.2.1#rendererImage: localhost:5000/grafana/grafana-image-renderer:3.2.1#g' "${OSM_HOME}"/charts/osm/values.yaml
 sed -i 's#image: jaegertracing/all-in-one#image: localhost:5000/jaegertracing/all-in-one#g' "${OSM_HOME}"/charts/osm/values.yaml
-sed -i 's#"flomesh/pipy-repo:latest"#"localhost:5000/flomesh/pipy-repo:latest"#g' "${OSM_HOME}"/charts/osm/templates/osm-deployment.yaml
-sed -i 's#"flomesh/pipy-repo-nightly:latest"#"localhost:5000/flomesh/pipy-repo-nightly:latest"#g' "${OSM_HOME}"/charts/osm/templates/osm-deployment.yaml
+sed -i 's#"flomesh/pipy-repo:latest"#"localhost:5000/flomesh/pipy-repo:latest"#g' "${OSM_HOME}"/charts/osm/values.yaml
+sed -i 's#"flomesh/pipy-repo-nightly:latest"#"localhost:5000/flomesh/pipy-repo-nightly:latest"#g' "${OSM_HOME}"/charts/osm/values.yaml
