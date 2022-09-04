b0b0051239a7d44658e3c52e2dc45febda94222a
Cybwan <baili@flomesh.io>
2022-09-04 09:44:14 +0800

    Update scripts

--- scripts/enable-test-demo-build.sh
+++ scripts/enable-test-demo-build.sh
@@ -9,6 +9,7 @@ fi
 
 OSM_HOME=$1
 
+make .env
 sed -i 's/^#make build-osm/make build-osm/g' "${OSM_HOME}"/demo/run-osm-demo.sh
 sed -i 's/^kind-demo: .env kind-up$/kind-demo: .env kind-up clean-osm/g' "${OSM_HOME}"/Makefile
 sed -i 's/^export PUBLISH_IMAGES=true/#export PUBLISH_IMAGES=false/g' "${OSM_HOME}"/.env
