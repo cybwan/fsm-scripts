f85ade4e24b9ff4db1ce47fc82f44a260ca93726
Cybwan <baili@flomesh.io>
2022-11-14 12:28:24 +0800

    Update scripts.

--- scripts/reload-flomesh-fsm-images.sh
+++ scripts/reload-flomesh-fsm-images.sh
@@ -7,8 +7,8 @@ IMG="flomesh/wait-for-it:1.2.0"; docker rmi "${IMG}" "localhost:5000/${IMG}"
 IMG="flomesh/toolbox:1.2.0"; docker rmi "${IMG}" "localhost:5000/${IMG}"
 IMG="flomesh/curl:7.84.0"; docker rmi "${IMG}" "localhost:5000/${IMG}"
 IMG="flomesh/mirrored-klipper-lb:v0.3.5"; docker rmi "${IMG}" "localhost:5000/${IMG}"
-IMG="flomesh/fsm-ingress-pipy:0.2.0-alpha.4"; docker rmi "${IMG}" "localhost:5000/${IMG}"
-IMG="flomesh/fsm-manager:0.2.0-alpha.4"; docker rmi "${IMG}" "localhost:5000/${IMG}"
+IMG="flomesh/fsm-ingress-pipy:0.2.0-alpha.4-dev"; docker rmi "${IMG}" "localhost:5000/${IMG}"
+IMG="flomesh/fsm-manager:0.2.0-alpha.4-dev"; docker rmi "${IMG}" "localhost:5000/${IMG}"
 
 docker container prune -f
 docker volume prune -f
@@ -22,8 +22,8 @@ docker exec $REGPOD rm -rf /var/lib/registry/docker/registry/v2/repositories/flo
 docker exec $REGPOD rm -rf /var/lib/registry/docker/registry/v2/repositories/flomesh/curl:7.84.0
 docker exec $REGPOD rm -rf /var/lib/registry/docker/registry/v2/repositories/flomesh/mirrored-klipper-lb:v0.3.5
 
-docker exec $REGPOD rm -rf /var/lib/registry/docker/registry/v2/repositories/flomesh/fsm-ingress-pipy:0.2.0-alpha.4
-docker exec $REGPOD rm -rf /var/lib/registry/docker/registry/v2/repositories/flomesh/fsm-manager:0.2.0-alpha.4
+docker exec $REGPOD rm -rf /var/lib/registry/docker/registry/v2/repositories/flomesh/fsm-ingress-pipy:0.2.0-alpha.4-dev
+docker exec $REGPOD rm -rf /var/lib/registry/docker/registry/v2/repositories/flomesh/fsm-manager:0.2.0-alpha.4-dev
 docker restart $REGPOD
 
 docker pull docker.io/flomesh/pipy:0.50.0-146
@@ -32,8 +32,8 @@ docker pull docker.io/flomesh/toolbox:1.2.0
 docker pull docker.io/flomesh/curl:7.84.0
 docker pull docker.io/flomesh/mirrored-klipper-lb:v0.3.5
 
-docker pull docker.io/flomesh/fsm-ingress-pipy:0.2.0-alpha.4
-docker pull docker.io/flomesh/fsm-manager:0.2.0-alpha.4
+docker pull docker.io/flomesh/fsm-ingress-pipy:0.2.0-alpha.4-dev
+docker pull docker.io/flomesh/fsm-manager:0.2.0-alpha.4-dev
 
 docker tag docker.io/flomesh/pipy:0.50.0-146 localhost:5000/flomesh/pipy:0.50.0-146
 docker tag docker.io/flomesh/wait-for-it:1.2.0 localhost:5000/flomesh/wait-for-it:1.2.0
@@ -41,8 +41,8 @@ docker tag docker.io/flomesh/toolbox:1.2.0 localhost:5000/flomesh/toolbox:1.2.0
 docker tag docker.io/flomesh/curl:7.84.0 localhost:5000/flomesh/curl:7.84.0
 docker tag docker.io/flomesh/mirrored-klipper-lb:v0.3.5 localhost:5000/flomesh/mirrored-klipper-lb:v0.3.5
 
-docker tag docker.io/flomesh/fsm-ingress-pipy:0.2.0-alpha.4 localhost:5000/flomesh/fsm-ingress-pipy:0.2.0-alpha.4
-docker tag docker.io/flomesh/fsm-manager:0.2.0-alpha.4 localhost:5000/flomesh/fsm-manager:0.2.0-alpha.4
+docker tag docker.io/flomesh/fsm-ingress-pipy:0.2.0-alpha.4-dev localhost:5000/flomesh/fsm-ingress-pipy:0.2.0-alpha.4-dev
+docker tag docker.io/flomesh/fsm-manager:0.2.0-alpha.4-dev localhost:5000/flomesh/fsm-manager:0.2.0-alpha.4-dev
 
 docker push localhost:5000/flomesh/pipy:0.50.0-146
 docker push localhost:5000/flomesh/wait-for-it:1.2.0
@@ -50,8 +50,8 @@ docker push localhost:5000/flomesh/toolbox:1.2.0
 docker push localhost:5000/flomesh/curl:7.84.0
 docker push localhost:5000/flomesh/mirrored-klipper-lb:v0.3.5
 
-docker push localhost:5000/flomesh/fsm-ingress-pipy:0.2.0-alpha.4
-docker push localhost:5000/flomesh/fsm-manager:0.2.0-alpha.4
+docker push localhost:5000/flomesh/fsm-ingress-pipy:0.2.0-alpha.4-dev
+docker push localhost:5000/flomesh/fsm-manager:0.2.0-alpha.4-dev
 
 IMG="flomesh/pipy:0.50.0-146"; docker rmi "${IMG}" "localhost:5000/${IMG}"
 IMG="flomesh/wait-for-it:1.2.0"; docker rmi "${IMG}" "localhost:5000/${IMG}"
@@ -59,8 +59,8 @@ IMG="flomesh/toolbox:1.2.0"; docker rmi "${IMG}" "localhost:5000/${IMG}"
 IMG="flomesh/curl:7.84.0"; docker rmi "${IMG}" "localhost:5000/${IMG}"
 IMG="flomesh/mirrored-klipper-lb:v0.3.5"; docker rmi "${IMG}" "localhost:5000/${IMG}"
 
-IMG="flomesh/fsm-ingress-pipy:0.2.0-alpha.4"; docker rmi "${IMG}" "localhost:5000/${IMG}"
-IMG="flomesh/fsm-manager:0.2.0-alpha.4"; docker rmi "${IMG}" "localhost:5000/${IMG}"
+IMG="flomesh/fsm-ingress-pipy:0.2.0-alpha.4-dev"; docker rmi "${IMG}" "localhost:5000/${IMG}"
+IMG="flomesh/fsm-manager:0.2.0-alpha.4-dev"; docker rmi "${IMG}" "localhost:5000/${IMG}"
 
 docker container prune -f
 docker volume prune -f
