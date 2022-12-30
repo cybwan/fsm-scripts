1b40efeb3b3124892382b4acb3f846dd21ca6914
Cybwan <baili@flomesh.io>
2022-12-30 13:26:43 +0800

    Update scripts.

--- scripts/reload-flomesh-fsm-images.sh
+++ scripts/reload-flomesh-fsm-images.sh
@@ -6,11 +6,11 @@ source ./scripts/funcs.sh
 
 images=(
 "flomesh/wait-for-it:1.2.0"
-"flomesh/toolbox:1.2.0"
+"flomesh/toolbox:1.3.0"
 "flomesh/curl:7.84.0"
 "flomesh/mirrored-klipper-lb:v0.3.5"
-"flomesh/fsm-ingress-pipy:0.2.0-alpha.10"
-"flomesh/fsm-manager:0.2.0-alpha.10"
+"flomesh/fsm-ingress-pipy:0.2.0-alpha.16"
+"flomesh/fsm-manager:0.2.0-alpha.16"
 )
 
 docker_io_images_to_local_registry "${images[@]}"
