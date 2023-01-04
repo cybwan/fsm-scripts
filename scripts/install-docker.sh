b477ec0017cc777c5e2ced4976514c508c0060c0
Cybwan <baili@flomesh.io>
2023-01-04 19:58:17 +0800

    Update scripts.

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -2,7 +2,8 @@
 
 sudo apt -y update
 sudo apt -y remove docker docker-engine docker.io containerd runc
-
+sudo apt -y install ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
@@ -10,7 +11,6 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt -y update
-sudo apt -y install ca-certificates curl gnupg lsb-release
 sudo apt -y install docker-ce docker-ce-cli containerd.io
 
 sudo groupadd docker
