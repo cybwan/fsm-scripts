add00e99c4caf5abdf2debcd87f9a50aad5df5bc
Cybwan <baili@flomesh.io>
2023-02-11 22:09:53 +0800

    Update scripts.

--- scripts/install-k8s-node-init-pull-images.sh
+++ scripts/install-k8s-node-init-pull-images.sh
@@ -13,7 +13,7 @@ docker pull registry.k8s.io/etcd:3.5.6-0
 docker pull registry.k8s.io/coredns/coredns:v1.8.6
 docker pull docker.io/calico/cni:v3.25.0
 docker pull docker.io/calico/node:v3.25.0
-
+docker pull docker.io/calico/kube-controllers:v3.25.0
 
 docker tag registry.k8s.io/kube-apiserver:v1.24.10          ${LOCAL_REGISTRY}/kube-apiserver:v1.24.10
 docker tag registry.k8s.io/kube-controller-manager:v1.24.10 ${LOCAL_REGISTRY}/kube-controller-manager:v1.24.10
@@ -22,9 +22,9 @@ docker tag registry.k8s.io/kube-proxy:v1.24.10              ${LOCAL_REGISTRY}/ku
 docker tag registry.k8s.io/pause:3.7                        ${LOCAL_REGISTRY}/pause:3.7
 docker tag registry.k8s.io/etcd:3.5.6-0                     ${LOCAL_REGISTRY}/etcd:3.5.6-0
 docker tag registry.k8s.io/coredns/coredns:v1.8.6           ${LOCAL_REGISTRY}/coredns:v1.8.6
-docker tag docker.io/calico/cni:v3.25.0                     ${LOCAL_REGISTRY}/cni:v3.25.0
-docker tag docker.io/calico/node:v3.25.0                    ${LOCAL_REGISTRY}/node:v3.25.0
-
+docker tag docker.io/calico/cni:v3.25.0                     ${LOCAL_REGISTRY}/calico/cni:v3.25.0
+docker tag docker.io/calico/node:v3.25.0                    ${LOCAL_REGISTRY}/calico/node:v3.25.0
+docker tag docker.io/calico/kube-controllers:v3.25.0        ${LOCAL_REGISTRY}/calico/kube-controllers:v3.25.0
 
 docker push ${LOCAL_REGISTRY}/kube-apiserver:v1.24.10
 docker push ${LOCAL_REGISTRY}/kube-controller-manager:v1.24.10
@@ -33,5 +33,6 @@ docker push ${LOCAL_REGISTRY}/kube-proxy:v1.24.10
 docker push ${LOCAL_REGISTRY}/pause:3.7
 docker push ${LOCAL_REGISTRY}/etcd:3.5.6-0
 docker push ${LOCAL_REGISTRY}/coredns:v1.8.6
-docker push ${LOCAL_REGISTRY}/cni:v3.25.0
-docker push ${LOCAL_REGISTRY}/node:v3.25.0
+docker push ${LOCAL_REGISTRY}/calico/cni:v3.25.0
+docker push ${LOCAL_REGISTRY}/calico/node:v3.25.0
+docker push ${LOCAL_REGISTRY}/calico/kube-controllers:v3.25.0
