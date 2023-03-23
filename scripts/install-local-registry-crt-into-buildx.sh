e4dde3aa811052821a466e649baf49eb160d4ecc
Cybwan <baili@flomesh.io>
2023-03-23 16:13:43 +0800

    Update scripts.

--- scripts/install-local-registry-crt-into-buildx.sh
+++ scripts/install-local-registry-crt-into-buildx.sh
@@ -4,8 +4,19 @@ set -euo pipefail
 
 echo -n | openssl s_client -showcerts -connect local.registry:443 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > local.registry.crt
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain local.registry.crt
-BUILDX=`docker ps | grep buildx | awk '{print $1}'`
-docker cp $BUILDX://etc/ssl/certs/ca-certificates.crt ca-certificates.crt
-cat local.registry.crt >> ca-certificates.crt
-docker cp ca-certificates.crt $BUILDX://etc/ssl/certs/ca-certificates.crt
-docker restart $BUILDX
+
+BUILDX=`docker ps | grep buildx_buildkit_ecnet0 | awk '{print $1}'`
+if [ "${BUILDX}"z != ""z ]; then
+  docker cp $BUILDX://etc/ssl/certs/ca-certificates.crt ca-certificates.crt
+  cat local.registry.crt >> ca-certificates.crt
+  docker cp ca-certificates.crt $BUILDX://etc/ssl/certs/ca-certificates.crt
+  docker restart $BUILDX
+fi
+
+BUILDX=`docker ps | grep buildx_buildkit_osm0 | awk '{print $1}'`
+if [ "${BUILDX}"z != ""z ]; then
+  docker cp $BUILDX://etc/ssl/certs/ca-certificates.crt ca-certificates.crt
+  cat local.registry.crt >> ca-certificates.crt
+  docker cp ca-certificates.crt $BUILDX://etc/ssl/certs/ca-certificates.crt
+  docker restart $BUILDX
+fi
