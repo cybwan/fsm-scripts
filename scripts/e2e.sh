249af00f30d37ea851429fd18cc47e4d9e076bd0
Cybwan <baili@flomesh.io>
2023-05-09 07:40:39 +0800

    Update scripts.

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -79,7 +79,7 @@ CNT=${#allCases[*]}
 No=1
 # shellcheck disable=SC2068
 for item in "${allCases[@]}"; do
-  echo -e "$(date '+%Y-%m-%d %H:%M:%S') Testing[${No}/${CNT}] $item ..."
+  echo -e "$(date '+%Y-%m-%d %H:%M:%S') Testing[${No}/${CNT}] [$item] ..."
   E2E_FLAGS="-ginkgo.focus='$item'" make test-e2e 2>/dev/null | grep 'Passed.*Failed.*Skipped'
   ((No=No+1))
 done
