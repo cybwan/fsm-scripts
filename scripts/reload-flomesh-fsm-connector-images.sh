c73e357ad8d74d4154d7e7f00db941bfce3b4ba4
Cybwan <baili@flomesh.io>
2024-03-22 13:30:53 +0800

    update scripts

--- scripts/reload-flomesh-fsm-connector-images.sh
+++ scripts/reload-flomesh-fsm-connector-images.sh
@@ -9,6 +9,7 @@ images=(
 "consul:1.15.4"
 "flomesh/samples-discovery-server:latest"
 "nacos/nacos-server:v2.3.0-slim"
+"cybwan/springboot-demo:latest"
 )
 
 docker_io_images_to_local_registry "${images[@]}"
