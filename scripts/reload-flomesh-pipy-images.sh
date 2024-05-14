7eeffeaf8146c57707966ac3f134afea79136c3a
Cybwan <baili@flomesh.io>
2024-05-14 10:20:12 +0800

    update scripts

--- scripts/reload-flomesh-pipy-images.sh
+++ scripts/reload-flomesh-pipy-images.sh
@@ -9,9 +9,9 @@ images=(
 "flomesh/pipy-nightly:latest"
 "flomesh/pipy-repo:latest"
 "flomesh/pipy-repo-nightly:latest"
-"flomesh/pipy:1.1.0-1
+"flomesh/pipy:1.1.0-1"
 "flomesh/pipy:1.1.0-1-1-nonroot"
-"flomesh/pipy-repo:1.1.0-1
+"flomesh/pipy-repo:1.1.0-1"
 )
 
 docker_io_images_to_local_registry "${images[@]}"
