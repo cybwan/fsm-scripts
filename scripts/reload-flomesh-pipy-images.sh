0c4f9ce4fbbacf55183b89bba0b7efad207b2ecd
Cybwan <baili@flomesh.io>
2024-05-14 10:23:18 +0800

    update scripts

--- scripts/reload-flomesh-pipy-images.sh
+++ scripts/reload-flomesh-pipy-images.sh
@@ -4,14 +4,16 @@ set -uo pipefail
 
 source ./scripts/funcs.sh
 
+PIPY_TAG="1.1.0-1"
+
 images=(
 "flomesh/pipy:latest"
 "flomesh/pipy-nightly:latest"
 "flomesh/pipy-repo:latest"
 "flomesh/pipy-repo-nightly:latest"
-"flomesh/pipy:1.1.0-1"
-"flomesh/pipy:1.1.0-1-1-nonroot"
-"flomesh/pipy-repo:1.1.0-1"
+"flomesh/pipy:${PIPY_TAG}"
+"flomesh/pipy:${PIPY_TAG}-nonroot"
+"flomesh/pipy-repo:${PIPY_TAG}"
 )
 
 docker_io_images_to_local_registry "${images[@]}"
